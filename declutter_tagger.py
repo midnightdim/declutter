da5563e3505f0047998fe98411a0653ea2502b35
Dmitry Beloglazov <dmitry.beloglazov@gmail.com>
2021-02-06 01:38:33 +0700

    tagger updated

--- declutter_tagger.py
+++ declutter_tagger.py
@@ -1,10 +1,11 @@
 import sys
-from PySide6.QtGui import QIcon, QColor, QCursor
-from PySide6.QtWidgets import QWidget, QApplication, QMainWindow, QFileSystemModel, QFileIconProvider, QMenu, QAbstractItemView, QWidgetAction
+from PySide6.QtGui import QIcon, QColor, QCursor, QAction
+from PySide6.QtWidgets import QWidget, QApplication, QMainWindow, QFileSystemModel, QFileIconProvider, QMenu, QAbstractItemView, QWidgetAction, QHBoxLayout, QLabel, QCheckBox
 from PySide6.QtCore import QObject, QDir, Qt, QModelIndex
 from ui_tagger_window import Ui_taggerWindow
 from declutter_lib import *
 from os.path import normpath
+from pathlib import Path
 
 class TaggerWindow(QMainWindow):
     def __init__(self):
@@ -18,10 +19,18 @@ class TaggerWindow(QMainWindow):
 
     def populate(self):
         path = r"D:\DIM\WinFiles\Downloads"
+        #path = str(Path.home() / "Downloads")
+        #path = r"G:\QUIK_VTB"
+        #path = ""
+        #path = r"D:\DIM\WinFiles\Downloads"
+        #path = normpath(r"D:\Projects.other\Programming\DeClutter archive\test")
+        #path = Path(path)
+        #print(path)
         self.model = TagFSModel()
         #self.model = QFileSystemModel()
-        self.model.setRootPath((QDir.rootPath()))
-        self.model.sort(0,Qt.SortOrder.DescendingOrder)
+        self.model.setRootPath(path)
+        self.model.setFilter(QDir.AllEntries | QDir.NoDot)
+        self.model.sort(0,Qt.SortOrder.AscendingOrder)
         #self.model.setFilter(QDir.AllDirs | QDir.NoDot)
         #self.model.sort(QDir.DirsFirst)
         self.ui.treeView.setModel(self.model)
@@ -29,11 +38,22 @@ class TaggerWindow(QMainWindow):
         self.model.setIconProvider(QFileIconProvider())
         self.ui.treeView.setItemsExpandable(False)
         self.ui.treeView.header().resizeSection(0,350)
-        #self.ui.treeView.header().setSortIndicator(1, Qt.DescendingOrder)
+        self.ui.treeView.header().setSortIndicator(0, Qt.AscendingOrder)
         self.ui.treeView.setSortingEnabled(True)
         self.ui.treeView.setRootIsDecorated(False)
         self.ui.treeView.setSelectionMode(QAbstractItemView.ExtendedSelection)
         #self.ui.treeView.resizeColumnToContents(0)  # doesn't work for some reason
+        self.ui.treeView.doubleClicked.connect(self.open)
+        self.checkAction = {}
+
+    def open(self):
+        index = self.ui.treeView.currentIndex()
+        file_path = normpath(self.model.filePath(index))
+        if os.path.isdir(file_path):
+            self.model.setRootPath(file_path)
+            self.ui.treeView.setRootIndex(self.model.index(file_path))
+        elif os.path.isfile(file_path):
+            os.startfile(file_path)
 
     def context_menu(self, position):
         index = self.ui.treeView.currentIndex()
@@ -41,16 +61,37 @@ class TaggerWindow(QMainWindow):
         file_tags = get_tags(file_path)
         #print(file_tags)
         menu = QMenu()
-        open = menu.addAction(file_path)
+        #open = menu.addAction(file_path)
+        #checkAction = CheckBoxAction(self,"test")
+        #checkAction.checkbox.setTristate()
+        #open = menu.addAction(checkAction)
+        
+        self.checkAction = {}
         for t in get_all_tags():
-            open = menu.addAction(t)
-            open.setCheckable(True)
+            self.checkAction[t] = CheckBoxAction(self,t)           
+            #self.checkAction[t].checkbox.checked.connect(self.test)
+            #self.checkAction[t].checkbox.unchecked.connect(self.test)
+            self.checkAction[t].checkbox.stateChanged.connect(self.test)
+            #open = menu.addAction(t)
+            #open.setCheckable(True)
             if t in file_tags:
-                open.setChecked(True)
+                #open.setChecked(True)
+                self.checkAction[t].checkbox.setChecked(True)                
+            menu.addAction(self.checkAction[t])
             #open.triggered.connect(self.tag_files)
         cursor = QCursor()
         menu.exec_(cursor.pos())
 
+    def test(self, state):
+        sender = self.sender()
+        #print("clicked",state)
+        index = self.ui.treeView.currentIndex()
+        file_path = normpath(self.model.filePath(index))        
+        if state == 2: #checked
+            print("tagged",file_path,"with",sender.text())
+            add_tag(file_path,sender.text())
+        elif state == 0: #unchecked
+            remove_tag(file_path,sender.text())
     def tag_files(self):
         print("tagging")
 
@@ -62,7 +103,7 @@ class TagFSModel(QFileSystemModel):
         if index.column() == self.columnCount() - 1:
             if role == Qt.DisplayRole:
                 #return self.fileName(index)
-                return ','.join(get_tags(normpath(self.filePath(index))))
+                return ', '.join(get_tags(normpath(self.filePath(index))))
             if role == Qt.TextAlignmentRole:
                 return Qt.AlignLeft
         if role == Qt.BackgroundRole and get_tags(normpath(self.filePath(index))):
@@ -70,9 +111,31 @@ class TagFSModel(QFileSystemModel):
 
         return super(TagFSModel, self).data(index, role)
 
-#class CheckBoxAction(QWidgetAction):
+class CheckBoxAction(QWidgetAction):
+    def __init__(self, parent, text):
+        #print(text)
+        #super(CheckBoxAction, parent=QAction).__init__()
+        super(CheckBoxAction, self).__init__(parent)
+        layout = QHBoxLayout()       
+        self.widget = QWidget()
+        #self.tag = text
+        #label = QLabel(text)
+        #label.setAlignment(Qt.AlignLeft)
+        self.checkbox = QCheckBox()
+        self.checkbox.setText(text)
+        #self.checkbox.setTristate()
+        layout.addWidget(self.checkbox)
+        #layout.addWidget(label)
+        layout.addStretch()
+        layout.setContentsMargins(3,3,6,3)
+        
+        self.widget.setLayout(layout)
 
+        self.setDefaultWidget(self.widget)
 
+    # def createWidget(self,text):
+    #     print("testing")
+    #     return self.widget
 
 def main():
     app = QApplication(sys.argv)
---
+++ ui_tagger_window.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'tagger_window.ui'
+##
+## Created by: Qt User Interface Compiler version 6.0.0
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import *
+from PySide6.QtGui import *
+from PySide6.QtWidgets import *
+
+import DeClutter_rc
+
+class Ui_taggerWindow(object):
+    def setupUi(self, taggerWindow):
+        if not taggerWindow.objectName():
+            taggerWindow.setObjectName(u"taggerWindow")
+        taggerWindow.resize(868, 599)
+        icon = QIcon()
+        icon.addFile(u":/images/DeClutter.ico", QSize(), QIcon.Normal, QIcon.Off)
+        taggerWindow.setWindowIcon(icon)
+        self.actionManage_Tags = QAction(taggerWindow)
+        self.actionManage_Tags.setObjectName(u"actionManage_Tags")
+        self.centralwidget = QWidget(taggerWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.gridLayout = QGridLayout(self.centralwidget)
+        self.gridLayout.setObjectName(u"gridLayout")
+        self.treeView = QTreeView(self.centralwidget)
+        self.treeView.setObjectName(u"treeView")
+        self.treeView.setRootIsDecorated(False)
+        self.treeView.setItemsExpandable(False)
+        self.treeView.setSortingEnabled(True)
+        self.treeView.setExpandsOnDoubleClick(False)
+
+        self.gridLayout.addWidget(self.treeView, 0, 0, 1, 1)
+
+        taggerWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(taggerWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 868, 21))
+        self.menuOptions = QMenu(self.menubar)
+        self.menuOptions.setObjectName(u"menuOptions")
+        taggerWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(taggerWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        taggerWindow.setStatusBar(self.statusbar)
+
+        self.menubar.addAction(self.menuOptions.menuAction())
+        self.menuOptions.addAction(self.actionManage_Tags)
+
+        self.retranslateUi(taggerWindow)
+
+        QMetaObject.connectSlotsByName(taggerWindow)
+    # setupUi
+
+    def retranslateUi(self, taggerWindow):
+        taggerWindow.setWindowTitle(QCoreApplication.translate("taggerWindow", u"DeClutter: Tagger", None))
+        self.actionManage_Tags.setText(QCoreApplication.translate("taggerWindow", u"Manage Tags", None))
+        self.menuOptions.setTitle(QCoreApplication.translate("taggerWindow", u"Options", None))
+    # retranslateUi
+
