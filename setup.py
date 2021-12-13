3970e8023a7212078c8757cbdde4eb4dedfadd4c
Dmitry Beloglazov <dmitry.beloglazov@gmail.com>
2021-12-13 18:42:17 +0700

    Updated build procedure

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ import sys
 # exclude unneeded packages. More could be added. Has to be changed for
 # other programs.
 build_exe_options = {"excludes": ["tkinter", "PyQt5", "PySide6","pandas","matplotlib", "PySide2.Qt5WebEngineCore",
-                                  "scipy",
-                                  "numpy"],
+                                  "scipy", "numpy"],
+                     "include_files": ["DeClutter.ico", "DeClutter.iss"],
                      "optimize": 2}
 
 # Information about the program and build command. Has to be adjusted for
 # other programs
 setup(
     name="DeClutter",                           # Name of the program
-    version="1.12.1",                           # Version number
+    version="1.12.2",                           # Version number
     description="DeClutter: file organizer",    # Description
     options = {"build_exe": build_exe_options}, # <-- the missing line
     executables=[Executable("DeClutter.py",     # Executable python file
