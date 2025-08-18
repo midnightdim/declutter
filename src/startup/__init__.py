c67e399d11a3b78fffa97ac7cd4ac88bd4ca4f21
Dmitry Beloglazov <dmitry@codeyourdream.com>
2025-08-18 17:16:45 +0400

    Simplify the startup facade

--- src/startup/__init__.py
+++ src/startup/__init__.py
@@ -1,8 +1,17 @@
 import sys
+
 if sys.platform.startswith("win"):
-    from .windows import is_startup_enabled as is_enabled, enable_startup as enable, disable_startup as disable
+    from .windows import (
+        is_startup_enabled as is_enabled,
+        enable_startup as enable,
+        disable_startup as disable,
+    )
 elif sys.platform == "darwin":
-    from .macos import is_startup_enabled as is_enabled, enable_startup as enable, disable_startup as disable
+    from .macos import (
+        is_startup_enabled as is_enabled,
+        enable_startup as enable,
+        disable_startup as disable,
+    )
 else:
     def is_enabled() -> bool: return False
     def enable() -> bool: return False
